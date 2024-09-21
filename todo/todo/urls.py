a8877cf662c4e7b22c4d0b71e329c1ed8b0bf6dd
yd-03 <3nu4ydye57@gmail.com>
2024-09-21 10:27:38 +0900

    refactor: ページのURLパスを変更

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import TodoList, TodoDetail, TodoUpdate, TodoDelete, TodoCreate
 
 urlpatterns = [
-    path("", TodoList.as_view(), name="list"),
-    path("detail/<int:pk>/", TodoDetail.as_view(), name="detail"),
-    path("update/<int:pk>/", TodoUpdate.as_view(), name="update"),
-    path("delete/<int:pk>/", TodoDelete.as_view(), name="delete"),
-    path("create/", TodoCreate.as_view(), name="create"),
+    path("", TodoList.as_view(), name="todo_list"),
+    path("todo_detail/<int:pk>/", TodoDetail.as_view(), name="todo_detail"),
+    path("todo_update/<int:pk>/", TodoUpdate.as_view(), name="todo_update"),
+    path("todo_delete/<int:pk>/", TodoDelete.as_view(), name="todo_delete"),
+    path("todo_create/", TodoCreate.as_view(), name="todo_create"),
 ]
--- todo/todo/views.py
+++ todo/todo/views.py
@@ -4,30 +4,31 @@ from django.urls import reverse_lazy
 
 
 class TodoList(generic.ListView):
-    template_name = "todo/list.html"
+    template_name = "todo/todo_list.html"
     model = TodoModel
+    context_object_name = "todos"
 
 
 class TodoDetail(generic.DetailView):
-    template_name = "todo/detail.html"
+    template_name = "todo/todo_detail.html"
     model = TodoModel
 
 
 class TodoUpdate(generic.UpdateView):
-    template_name = "todo/update.html"
+    template_name = "todo/todo_update.html"
     model = TodoModel
     fields = ("title", "content", "deadline")
-    success_url = reverse_lazy("list")
+    success_url = reverse_lazy("todo_list")
 
 
 class TodoDelete(generic.DeleteView):
-    template_name = "todo/delete.html"
+    template_name = "todo/todo_delete.html"
     model = TodoModel
-    success_url = reverse_lazy("list")
+    success_url = reverse_lazy("todo_list")
 
 
 class TodoCreate(generic.CreateView):
-    template_name = "todo/create.html"
+    template_name = "todo/todo_create.html"
     model = TodoModel
     fields = ("title", "content", "deadline")
-    success_url = reverse_lazy("list")
+    success_url = reverse_lazy("todo_list")
