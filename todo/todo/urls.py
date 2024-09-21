881bd2ecfc196ca7fa9bfff20983f4ed80849966
yd-03 <3nu4ydye57@gmail.com>
2024-09-21 09:45:16 +0900

    feat: todoアプリケーションの詳細ページを追加

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TodoList
+from .views import TodoList, TodoDetail
 
 urlpatterns = [
     path("", TodoList.as_view(), name="list"),
+    path("detail/<int:pk>/", TodoDetail.as_view(), name="detail"),
 ]
--- todo/todo/views.py
+++ todo/todo/views.py
@@ -5,3 +5,8 @@ from .models import TodoModel
 class TodoList(generic.ListView):
     template_name = "todo/list.html"
     model = TodoModel
+
+
+class TodoDetail(generic.DetailView):
+    template_name = "todo/detail.html"
+    model = TodoModel
