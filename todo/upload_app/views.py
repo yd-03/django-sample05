bbba1bb3c7e8a6c000774c7059593f40526833b7
yd-03 <3nu4ydye57@gmail.com>
2024-09-25 08:56:59 +0900

    feat: メディアプレビュー用のViewの作成

--- todo/upload_app/views.py
+++ todo/upload_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .forms import UploadForm
+from .models import UploadImage
 
 
 def index(request):
@@ -14,3 +15,15 @@ def index(request):
             upload_image = form.save()
             params["id"] = upload_image.id
     return render(request, "upload_app/index.html", params)
+
+
+def preview(request, image_id=0):
+    upload_image = get_object_or_404(UploadImage, id=image_id)
+
+    params = {
+        "title": "画像の表示",
+        "id": upload_image.id,
+        "url": upload_image.image.url,
+    }
+
+    return render(request, "upload_app/preview.html", params)
