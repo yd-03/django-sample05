7e0b500362f75c8438ce98dcfdfaab877b659cd2
yd-03 <3nu4ydye57@gmail.com>
2024-09-25 08:36:23 +0900

    feat: 画像のアップロード機能を追加

--- todo/upload_app/views.py
+++ todo/upload_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .forms import UploadForm
 
-# Create your views here.
+
+def index(request):
+    params = {
+        "title": "画像のアップロード",
+        "upload_form": UploadForm(),
+        "id": None,
+    }
+    if request.method == "POST":
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            upload_image = form.save()
+            params["id"] = upload_image.id
+    return render(request, "upload_app/index.html", params)
