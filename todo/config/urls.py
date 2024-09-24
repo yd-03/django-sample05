de45b57e6267ff79c7774c3fafe2b3418fcdbc29
yd-03 <3nu4ydye57@gmail.com>
2024-09-25 08:39:42 +0900

    feat: メディアファイルのアップロード用の View と URL の関連付け

--- todo/config/urls.py
+++ todo/config/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("todo.urls")),
     path("accounts/", include("allauth.urls")),
+    path("upload_app/", include("upload_app.urls")),
 ]
