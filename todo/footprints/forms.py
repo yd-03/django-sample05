6ad6a9623367f4d8a6bb36d847458833b9130028
yd-03 <3nu4ydye57@gmail.com>
2024-09-30 22:48:02 +0900

    feat: ユーザーのサインアップとログインフォームを追加

---
+++ todo/footprints/forms.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+
+class LoginForm(AuthenticationForm):
+    pass
